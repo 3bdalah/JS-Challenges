602dbae9cbd337eef23ba2ed3165400a92652f3c
3bdalah <3bdallah.dev@gmail.com>
2023-05-06 03:18:19 +0300

    adding set compression problem

---
+++ set-compression.js
@@ -0,0 +1,21 @@
+const stringCompression = (str) => {
+      const n = str.length;
+      let count =1, output='',uniqCount =0;
+      for(let i=0;i<n;i++){
+        if(str[i] === str[i-1]){
+            count++;
+            
+        }else{
+            output += str[i]+count;
+            count=1;
+            uniqCount++;
+        }
+      }
+      if(uniqCount === n) return str;
+      else return output;
+};
+
+console.log({
+    "aaabbbccdddddeew":stringCompression("aaabbbccdddddeew"),
+    "monnbbbbfv":stringCompression("monnbbfv")
+});
